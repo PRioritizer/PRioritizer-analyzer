61068752ed103d395269074a7302d465ed50b5b9
Erik van der Veen <erikvdv1@gmail.com>
2014-05-15 17:12:59 +0200

    Order pull requests pairs

--- src/main/scala/git/PullRequest.scala
+++ src/main/scala/git/PullRequest.scala
@@ -1,15 +1,18 @@
 package git
 
+import scala.collection.immutable.SortedSet
+
 //case class PullRequest(branch: String, base:String, account:String, repo:String)
 case class PullRequest(number: Int, branch: String, base:String) {
   override def toString(): String = {
-    s"#$number: '$branch'"
+    s"#$number: '$branch' into '$base'"
   }
 }
 
 object PullRequest {
+  implicit val ord = Ordering.by[PullRequest, Int](_.number)
 
-  def getPairs(pulls: List[PullRequest]): Set[(PullRequest, PullRequest)] = {
+  def getPairs(pulls: List[PullRequest]): SortedSet[(PullRequest, PullRequest)] = {
     val pairs = for {
       // Pairwise
       x <- pulls
@@ -19,7 +22,8 @@ object PullRequest {
       pr1 = if (x.number < y.number) x else y
       pr2 = if (x.number < y.number) y else x
     } yield (pr1, pr2)
-    // Distinct
-    pairs.toSet
+
+    // Distinct and sort on base branch
+    SortedSet(pairs: _*)
   }
 }
