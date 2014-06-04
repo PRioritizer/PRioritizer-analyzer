80f96ffc39293b3830b9538a1a73b4f8e407a243
Erik van der Veen <erikvdv1@gmail.com>
2014-06-04 12:10:17 +0200

    Conditional JGit enrichment

--- src/main/scala/jgit/enrich/JGitEnrichmentProvider.scala
+++ src/main/scala/jgit/enrich/JGitEnrichmentProvider.scala
@@ -17,20 +17,29 @@ class JGitEnrichmentProvider(val provider: JGitProvider) extends EnrichmentProvi
 
   override def enrich(pullRequest: PullRequest): Future[PullRequest] = {
     Future {
-      val head = repo resolve pullRef(pullRequest)
-      val target = repo resolve targetRef(pullRequest)
-      val base = if (head != null && target != null) CommitUtils.getBase(repo, head, target) else null
-
-      // Check if commits are resolved
-      if (head != null && base != null) {
-        val Stats(added, edited, deleted, numFiles, numCommits) = repo.stats(head, base)
-        pullRequest.linesAdded = added + edited
-        pullRequest.linesDeleted = deleted + edited
-        pullRequest.filesChanged = numFiles
-        pullRequest.commits = numCommits
-      }
-
-      pullRequest
+      if (!hasStats(pullRequest))
+        enrichStats(pullRequest)
+      else
+        pullRequest
     }
+  }
+
+  private def hasStats(pullRequest: PullRequest): Boolean = pullRequest.commits > 0
+
+  private def enrichStats(pullRequest: PullRequest): PullRequest = {
+    val head = repo resolve pullRef(pullRequest)
+    val target = repo resolve targetRef(pullRequest)
+    val base = if (head != null && target != null) CommitUtils.getBase(repo, head, target) else null
+
+    // Check if commits are resolved
+    if (head != null && base != null) {
+      val Stats(added, edited, deleted, numFiles, numCommits) = repo.stats(head, base)
+      pullRequest.linesAdded = added + edited
+      pullRequest.linesDeleted = deleted + edited
+      pullRequest.filesChanged = numFiles
+      pullRequest.commits = numCommits
+    }
+
+    pullRequest
   }
 }
