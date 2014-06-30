28e37d8d7507e6994e1179ae7e4c53b4f5b62ad7
Erik van der Veen <erikvdv1@gmail.com>
2014-06-30 15:04:39 +0200

    Disable white space support in cli

--- run.bat
+++ run.bat
@@ -1,8 +1,15 @@
 @echo off
 
+set OWNER=%1
+set REPOSITORY=%2
+set GIT_LOCATION=%3
+
 IF "%3"=="" GOTO InvalidArgs
 IF NOT "%4"=="" GOTO InvalidArgs
-IF NOT EXIST "%3" GOTO DirNotFound
+IF NOT %OWNER: =%==%OWNER% GOTO Whitespace
+IF NOT %REPOSITORY: =%==%REPOSITORY% GOTO Whitespace
+IF NOT %GIT_LOCATION: =%==%GIT_LOCATION% GOTO Whitespace
+IF NOT EXIST %GIT_LOCATION% GOTO DirNotFound
 GOTO ValidArgs
 
 :InvalidArgs
@@ -12,14 +19,13 @@ goto end
 
 :DirNotFound
 echo Git repository does not exist.
-echo Usage: %0 owner_name repository_name git_location
+goto end
+
+:Whitespace
+echo Whitespace is not supported.
 goto end
 
 :ValidArgs
-set OWNER=%1
-set REPOSITORY=%2
-set GIT_LOCATION=%3
-
 set JAVA_OPTS=-Dfile.encoding=UTF8 -Dgithub.Owner="%OWNER%" -Dgithub.Repository="%REPOSITORY%" -Djgit.Directory="%GIT_LOCATION%"
 sbt run
 :end
--- run.sh
+++ run.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 
+OWNER=$1
+REPOSITORY=$2
+GIT_LOCATION=$3
+
 if [ "$#" -ne 3 ]; then
   echo "Wrong number of arguments, expected 3 arguments." >&2
   echo "Usage: $0 owner_name repository_name git_location" >&2
   exit 1
 fi
 
-if ! [ -d "$3" ]; then
+case "$OWNER$REPOSITORY$GIT_LOCATION" in
+  *\ * )
+  echo "Whitespace is not supported." >&2
+  exit 1
+  ;;
+esac
+
+if ! [ -d "$GIT_LOCATION" ]; then
   echo "Git repository does not exist." >&2
-  echo "Usage: $0 owner_name repository_name git_location" >&2
   exit 1
 fi
 
-OWNER=$1
-REPOSITORY=$2
-GIT_LOCATION=$3
-
-JAVA_OPTS="-Dfile.encoding=UTF8 -Dgithub.Owner=\"$OWNER\" -Dgithub.Repository=\"$REPOSITORY\" -Djgit.Directory=\"$GIT_LOCATION\""
+JAVA_OPTS="-Dfile.encoding=UTF8 -Dgithub.Owner=$OWNER -Dgithub.Repository=$REPOSITORY -Djgit.Directory=$GIT_LOCATION"
 sbt run $JAVA_OPTS
