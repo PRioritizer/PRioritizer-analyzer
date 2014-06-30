73091410bc1f8e65c3bdb58744b4174540e0bf42
Erik van der Veen <erikvdv1@gmail.com>
2014-06-30 13:51:55 +0200

    Fix command line shell script

--- run.sh
+++ run.sh
@@ -17,4 +17,4 @@ REPOSITORY=$2
 GIT_LOCATION=$3
 
 JAVA_OPTS="-Dfile.encoding=UTF8 -Dgithub.Owner=\"$OWNER\" -Dgithub.Repository=\"$REPOSITORY\" -Djgit.Directory=\"$GIT_LOCATION\""
-sbt run
+sbt run $JAVA_OPTS
