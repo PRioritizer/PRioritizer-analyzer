c0f88581b2346f222b32005f563613a598f03bdf
Erik van der Veen <erikvdv1@gmail.com>
2015-07-02 16:08:28 +0200

    Limit thread count by 4

--- run.bat
+++ run.bat
@@ -6,6 +6,7 @@ SET VERSION=1.0
 SET SCALA_VERSION=2.11
 SET D=%~dp0
 SET JAR=%D%\target\scala-%SCALA_VERSION%\%NAME%-assembly-%VERSION%.jar
+SET THREADS=4
 SET _argcActual=0
 FOR %%i in (%*) DO SET /A _argcActual+=1
 
@@ -40,7 +41,7 @@ ECHO Whitespace is not supported.
 GOTO end
 
 :ValidArgs
-SET PROPS=-Dfile.encoding=UTF8 -Dgithub.owner=%OWNER% -Dgithub.repository=%REPOSITORY% -Djgit.directory=%GIT_LOCATION%
+SET PROPS=-Dfile.encoding=UTF8 -Dgithub.owner=%OWNER% -Dgithub.repository=%REPOSITORY% -Djgit.directory=%GIT_LOCATION% -Dscala.concurrent.context.minThreads=%THREAD% -Dscala.concurrent.context.numThreads=%THREADS% -Dscala.concurrent.context.maxThreads=%THREADS%
 java %PROPS% -jar %JAR%
 :end
 
